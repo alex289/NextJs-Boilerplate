75e6d10a401a269a478adcfcbe8ab53c6fc74aa5
JuzouSatoru2 <mccr4fthd@gmail.com>
2020-07-28 19:35:24 +0200

    Add Nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,117 @@
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-}
+import Router from "next/router";
+import NProgress from "nprogress";
 
-export default MyApp
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        #nprogress {
+          pointer-events: none;
+        }
+        #nprogress .bar {
+          background: #29d;
+          position: fixed;
+          z-index: 1031;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 2px;
+        }
+        #nprogress .peg {
+          display: block;
+          position: absolute;
+          right: 0;
+          width: 100px;
+          height: 100%;
+          box-shadow: 0 0 10px #29d, 0 0 5px #29d;
+          opacity: 1;
+          -webkit-transform: rotate(3deg) translate(0, -4px);
+          -ms-transform: rotate(3deg) translate(0, -4px);
+          transform: rotate(3deg) translate(0, -4px);
+        }
+        #nprogress .spinner {
+          display: block;
+          position: fixed;
+          z-index: 1031;
+          top: 15px;
+          right: 15px;
+        }
+        #nprogress .spinner-icon {
+          width: 18px;
+          height: 18px;
+          box-sizing: border-box;
+          border: solid 2px transparent;
+          border-top-color: #29d;
+          border-left-color: #29d;
+          border-radius: 50%;
+          -webkit-animation: nprogress-spinner 0.4s linear infinite;
+          animation: nprogress-spinner 0.4s linear infinite;
+        }
+        .nprogress-custom-parent {
+          overflow: hidden;
+          position: relative;
+        }
+        .nprogress-custom-parent #nprogress .bar,
+        .nprogress-custom-parent #nprogress .spinner {
+          position: absolute;
+        }
+        @-webkit-keyframes nprogress-spinner {
+          0% {
+            -webkit-transform: rotate(0);
+          }
+          100% {
+            -webkit-transform: rotate(360deg);
+          }
+        }
+        @keyframes nprogress-spinner {
+          0% {
+            transform: rotate(0);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+        ::-webkit-scrollbar {
+          width: 3px;
+          height: 2px;
+        }
+        ::-webkit-scrollbar-button {
+          width: 0;
+          height: 0;
+        }
+        ::-webkit-scrollbar-thumb {
+          background: #fff;
+          border: 0 none #fff;
+          border-radius: 50px;
+        }
+        ::-webkit-scrollbar-thumb:hover {
+          background: #afafaf;
+        }
+        ::-webkit-scrollbar-thumb:active {
+          background: #707070;
+        }
+        ::-webkit-scrollbar-track {
+          background: #666;
+          border: 0 none #fff;
+          border-radius: 50px;
+        }
+        ::-webkit-scrollbar-track:hover {
+          background: #666;
+        }
+        ::-webkit-scrollbar-track:active {
+          background: #333;
+        }
+        ::-webkit-scrollbar-corner {
+          background: 0 0;
+        }
+      `}</style>
+    </div>
+  );
+}
