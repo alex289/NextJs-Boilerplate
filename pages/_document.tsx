eee169989240920e1d1a8bbca397066691dc7761
Alexander Konietzko <Alexander.Konietzko@netgo.de>
2021-08-19 13:36:37 +0200

    Add serverside stylesheet

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,10 @@ import Document, {
   DocumentContext,
 } from 'next/document';
 
+import { Children } from 'react';
+
+import { ServerStyleSheets } from '@material-ui/core/styles';
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -31,5 +35,25 @@ class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [
+      ...Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
 
 export default MyDocument;
