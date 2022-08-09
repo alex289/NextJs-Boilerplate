14b65b8ad6380a070f32367bfb015297a362210a
Alex289 <mccr4fthd@gmail.com>
2022-08-09 08:29:24 +0200

    refactor: :art: Improve next-auth config

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,12 @@ import GithubProvider from 'next-auth/providers/github';
 import type { NextAuthOptions } from 'next-auth';
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  theme: {
+    colorScheme: 'auto',
+    brandColor: '',
+    logo: 'https://next-js-boilerplate-sable.vercel.app/static/icon/favicon.ico',
+  },
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
