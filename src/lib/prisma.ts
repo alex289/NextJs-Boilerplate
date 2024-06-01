f3ae546eb0428067859678722dcdf26d12c1ec60
alex289 <mccr4fthd@gmail.com>
2024-06-01 13:10:21 +0200

    chore: Update deps

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,13 +3,15 @@ import { PrismaClient } from '@prisma/client';
 import { env } from '@/env.mjs';
 
 declare global {
-  // eslint-disable-next-line no-var
+  // eslint-disable-next-line no-var, @typescript-eslint/no-redundant-type-constituents
   var prisma: PrismaClient | undefined;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 export const prisma = global.prisma ?? new PrismaClient();
 
 if (env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   global.prisma = prisma;
 }
 
