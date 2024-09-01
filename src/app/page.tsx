b1c6270c28e1824ce9f971e737cca9fa7241ec42
alex289 <mccr4fthd@gmail.com>
2024-09-01 11:13:56 +0200

    feat: Add nuqs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,34 @@
 'use client';
 
+import { useQueryState } from 'nuqs';
 import { toast } from 'sonner';
 
 import { DialogExample } from '@/components/dialog-example';
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
 const Index = () => {
+  const [name, setName] = useQueryState('name');
   return (
     <>
-      <div className="flex justify-center">
+      <div className="flex justify-center my-4">
         <h1 className="text-5xl font-extrabold leading-normal text-gray-700 dark:text-gray-200 md:text-[5rem]">
           Next.Js <span className="text-indigo-500">Boilerplate</span>
         </h1>
       </div>
 
-      <div className="flex gap-4">
+      <div className="flex md:flex-row flex-col gap-4">
         <Button onClick={() => toast('Event has been created.')}>
           Toast example
         </Button>
 
         <DialogExample />
+
+        <Input
+          value={name ?? ''}
+          onChange={(e) => setName(e.target.value)}
+          className="w-50"
+        />
       </div>
     </>
   );
