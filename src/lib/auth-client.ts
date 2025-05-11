c0891bfa9a1ab3534c8ebb7e2278ddbde2705e10
alex289 <mccr4fthd@gmail.com>
2025-05-11 14:52:48 +0200

    fix: Does this work

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,5 @@
 import { createAuthClient } from 'better-auth/react';
 
-import { env } from '@/env.mjs';
-
 export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_VERCEL_URL,
+  // baseURL: env.NEXT_PUBLIC_VERCEL_URL,
 });
